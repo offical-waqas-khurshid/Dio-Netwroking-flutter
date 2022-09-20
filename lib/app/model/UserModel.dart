64f36cd3e7a43fdafa28939ccee8e10bd292eb08
git <git>
2022-09-20 17:51:49 +0500

    add model class

---
+++ lib/app/model/UserModel.dart
@@ -0,0 +1,26 @@
+
+import 'dart:convert';
+
+UserModel userModelFromJson(String str) => UserModel.fromJson(json.decode(str));
+
+String userModelToJson(UserModel data) => json.encode(data.toJson());
+
+class UserModel{
+  String? name;
+  String? email;
+  String? password;
+
+  UserModel({this.name, this.email, this.password});
+
+  factory UserModel.fromJson(Map<String, dynamic> json) => UserModel(
+    name: json["name"],
+    email: json["email"],
+    password: json["password"],
+      );
+
+  Map<String, dynamic> toJson() => {
+    "name": name,
+    "email": email,
+    "password": password,
+  };
+}
--- lib/app/modules/signup_page/views/signup_page_view.dart
+++ lib/app/modules/signup_page/views/signup_page_view.dart
@@ -7,6 +7,7 @@ import '../controllers/signup_page_controller.dart';
 
 class SignupPageView extends GetView<SignupPageController> {
   const SignupPageView({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -14,86 +15,97 @@ class SignupPageView extends GetView<SignupPageController> {
         title: const Text('SignupPageView'),
         centerTitle: true,
       ),
-      body:  Column(
+      body: Column(
         children: [
-             Form(
-               autovalidateMode: AutovalidateMode.onUserInteraction,
-               child: Padding(
-               padding: const EdgeInsets.all(15.0),
-               child: Column(
-                   children:   [
-                    const SizedBox(height: 25,),
-                    const Text('Signup', style: TextStyle(fontSize: 32, fontWeight: FontWeight.w700),),
-                     const SizedBox(height: 25,),
-                      TextFormField(
-                      decoration: const InputDecoration(
-                        border: OutlineInputBorder(),
-                        hintText: 'Name',
-                        labelText: 'Name',
-                      ),
-                       validator: (value) {
-                         var validator = Validator(
-                           validators: [RequiredValidator()],
-                         );
-
-                         return validator.validate(
-                           context: context,
-                           label: 'Required',
-                           value: value,
-                         );
-                       },
+          Form(
+            autovalidateMode: AutovalidateMode.onUserInteraction,
+            child: Padding(
+              padding: const EdgeInsets.all(15.0),
+              child: Column(
+                children: [
+                  const SizedBox(
+                    height: 25,
+                  ),
+                  const Text(
+                    'Signup',
+                    style: TextStyle(fontSize: 32, fontWeight: FontWeight.w700),
+                  ),
+                  const SizedBox(
+                    height: 25,
+                  ),
+                  TextFormField(
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      hintText: 'Name',
+                      labelText: 'Name',
                     ),
-                     const SizedBox(height: 25,),
-                      TextFormField(
-                       decoration: const InputDecoration(
-                         border: OutlineInputBorder(),
-                         hintText: 'Email',
-                         labelText: 'Email',
-                       ),
-                        validator: (value) {
-                          var validator = Validator(
-                            validators: [
-                              RequiredValidator(),
-                              EmailValidator(),
-                            ],
-                          );
+                    validator: (value) {
+                      var validator = Validator(
+                        validators: [RequiredValidator()],
+                      );
 
-                          return validator.validate(
-                            context: context,
-                            label: 'Email',
-                            value: value,
-                          );
-                        },
-                     ),
-                     const SizedBox(height: 25,),
-                      TextFormField(
-                       decoration: const InputDecoration(
-                         border: OutlineInputBorder(),
-                         hintText: 'Password',
-                         labelText: 'Password',
-                       ),
-                        validator: (value) {
-                          var validator = Validator(
-                            validators: [
-                              MaxLengthValidator(length: 15),
-                              MinLengthValidator(length: 6),
-                            ],
-                          );
+                      return validator.validate(
+                        context: context,
+                        label: 'Required',
+                        value: value,
+                      );
+                    },
+                  ),
+                  const SizedBox(
+                    height: 25,
+                  ),
+                  TextFormField(
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      hintText: 'Email',
+                      labelText: 'Email',
+                    ),
+                    validator: (value) {
+                      var validator = Validator(
+                        validators: [
+                          RequiredValidator(),
+                          EmailValidator(),
+                        ],
+                      );
 
-                          return validator.validate(
-                            context: context,
-                            label: 'Min 6 / Max 15 Length',
-                            value: value,
-                          );
-                        },
-                     ),
-                     const SizedBox(height: 25,),
-                     ElevatedButton(onPressed: (){
-
-                     }, child: const Text('Signup')),
-                   ],
-               ),
-             ),),
+                      return validator.validate(
+                        context: context,
+                        label: 'Email',
+                        value: value,
+                      );
+                    },
+                  ),
+                  const SizedBox(
+                    height: 25,
+                  ),
+                  TextFormField(
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      hintText: 'Password',
+                      labelText: 'Password',
+                    ),
+                    validator: (value) {
+                      var validator = Validator(
+                        validators: [
+                          MaxLengthValidator(length: 15),
+                          MinLengthValidator(length: 6),
+                        ],
+                      );
+                      return validator.validate(
+                        context: context,
+                        label: 'Min 6 / Max 15 Length',
+                        value: value,
+                      );
+                    },
+                  ),
+                  const SizedBox(
+                    height: 25,
+                  ),
+                  ElevatedButton(onPressed: () {}, child: const Text('Signup')),
+                ],
+              ),
+            ),
+          ),
         ],
       ),
     );
